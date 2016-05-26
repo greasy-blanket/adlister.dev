285c789bceb34557e8db8dcba628c02b4e1c3cfb
kcates81 <kriscates81@gmail.com>
2016-05-26 15:29:51 -0500

    created the migration for the ad table

--- database/migrations/ads_migration.php
+++ database/migrations/ads_migration.php
@@ -2,15 +2,17 @@
     $_ENV = include __DIR__ . '/../../.env.php';
     require_once '../db_connect.php';
 
-    $dbc->exec('DROP TABLE IF EXISTS users');
+    $dbc->exec('DROP TABLE IF EXISTS ads');
 
-    $query = 'CREATE TABLE users (
+    $query = 'CREATE TABLE ads (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        name VARCHAR(240) NOT NULL,
-        description VARCHAR(240) NOT NULL,
-        price VARCHAR(100) NOT NULL,
+        user_id INT UNSIGNED NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        price DECIMAL(10,2) NOT NULL,
         img_url VARCHAR(255) NOT NULL,
-        PRIMARY KEY (id)
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES users (id)
     )';
 
     $dbc->exec($query);
