efb5fa286a5a5d89ee9868e17d89a3f2f63b2752
Will Campbell <sufuninja@gmail.com>
2016-05-26 10:33:56 -0500

    Updated code and corrected indentions

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,15 +1,17 @@
-<!--partial view for navbar-->
+<!-- materialize nav bar.  logo on the left buttons on the right -->
 <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="home_file.html">Home</a></li>
-        <li><a href="items_file.html">Items</a></li>
-        <li><a href="account_file.html">Account</a></li>
-        <li><a href="login_file.html">Login</a></li>
-        <li><a href="logout_file.html">Log Out</a></li>        
-        <li><a href="signup_file.html">Sign Up</a></li>
-        <li><a href="postad_file.html">Post New Add</a></li>
-      </ul>
+    <!--logo picture-->
+        <a class="brand-logo"><img src="/img/adlister-logo-small.png"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="homepage.html">Home</a></li>
+            <li><a href="index.php">Items</a></li>
+            <li><a href="account.php">Account</a></li>
+            <li><a href="login.php">Login</a></li>
+    <!--unsure where the logout button takes the user  -->
+            <li><a href="logout.html">Log Out</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="create.php">Post New Add</a></li>
+        </ul>
     </div>
-  </nav>
+</nav>
