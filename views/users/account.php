a223deaef0e536b05c3d89c3883ceafaa33db649
Will Campbell <sufuninja@gmail.com>
2016-05-26 09:36:30 -0500

    changed form to use foreach loop and echoed variables for future data

--- views/users/account.php
+++ views/users/account.php
@@ -10,8 +10,42 @@
 
 </head>
 <body>
-  <?php //require 'nav-bar'; ?>
+    <?php //require 'nav-bar'; ?>
+    <div class="container">
+        <div class="user-info section center-align">
+            <h2>User Info</h2>
 
-  <?php //require 'js-files'; ?>
+            <div class="info left-align">
+                <div class="name">
+                    <p>Name: <?= //$userinfo['name'] ?></p>
+                </div>
+
+                <div class="email">
+                    <p>Email: <?= //$userinfo['email'] ?></p>
+                </div>
+            </div>
+            <button class="edit-profile btn">Edit Profile</button>
+        </div><!-- End of user-info -->
+
+        <div class="user-ads section center-align">
+            <h2>Your Ads</h2>
+
+            <div class="info">
+                <?php foreach($ads as $adTitle): ?>
+
+                    <div class="ad-title">
+                        <p><?= $adTitle ?></p>
+                    </div>
+
+                <?php endforeach ?>
+
+            </div>
+            <button class="create-ad btn">Create Ad</button>
+        </div><!-- End of user-ads -->
+    </div><!-- End of container -->
+
+    <?php //require 'footer.php'; ?>
+
+    <?php //require 'js-files'; ?>
 </body>
 </html>
