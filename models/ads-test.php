8d0ebd09c7b0fd5520ae4e6cf28523056092f566
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-26 15:59:12 -0500

    initial test built.  adding works.  updating makes a new recor and updates but adds a new record instead of only changing

--- models/ads-test.php
+++ models/ads-test.php
@@ -1,4 +1,17 @@
 <?php
 
-require '/Ad.php';
+require 'Ad.php';
 
+// making a new add
+$ads = new Ad();
+$ads->user_id = '1';
+$ads->name = 'Topo Chico';
+$ads->description = 'Case of 24 carbonated mineral waters imported from Mexico';
+$ads->price = '20.00';
+$ads->img_url = 'http://bit.ly/1TExKS6';
+$ads->save();
+var_dump($ads->attributes);
+
+$ads->name = 'Bubble Water';
+$ads->save();
+var_dump($ads);
