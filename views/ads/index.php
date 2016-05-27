603034674f492ecf3df8ea4f39a300fcc412a06f
kcates81 <kriscates81@gmail.com>
2016-05-27 10:22:13 -0500

    put a container div around all of the page content

--- views/ads/index.php
+++ views/ads/index.php
@@ -1,28 +1,30 @@
-<div>
-    <h1 class="center-align">Items Page</h1>
-</div>
+<div class="container">
+    <div>
+        <h1 class="center-align">Items Page</h1>
+    </div>
 
-<section id="items">
-    <?php foreach ($ads as $index=>$ad): ?>
-        <?php if ($index % 4 == 0): ?>
-            <div class="row">
-        <?php endif ?>    
-                <div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="<?= $ad['img_url']; ?>" >
-                            <span class="card-title">Item <?= $ad['name']; ?></span>
-                        </div>
-                        <div class="card-content">
-                            <p><?= $ad['description']; ?></p>
-                        </div>
-                        <div class="card-action">
-                            <a href=""><button class="btn">Details Page</button></a>
+    <section id="items">
+        <?php foreach ($ads as $index=>$ad): ?>
+            <?php if ($index % 4 == 0): ?>
+                <div class="row">
+            <?php endif ?>    
+                    <div class="col s12 m3">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="<?= $ad['img_url']; ?>" >
+                                <span class="card-title">Item <?= $ad['name']; ?></span>
+                            </div>
+                            <div class="card-content">
+                                <p><?= $ad['description']; ?></p>
+                            </div>
+                            <div class="card-action">
+                                <a href=""><button class="btn">Details Page</button></a>
+                            </div>
                         </div>
                     </div>
+            <?php if ($index % 4 == 3): ?>
                 </div>
-        <?php if ($index % 4 == 3): ?>
-            </div>
-        <?php endif ?>    
-    <?php endforeach; ?>
-</section>
+            <?php endif ?>    
+        <?php endforeach; ?>
+    </section>   
+</div>
