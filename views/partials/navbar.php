2ac546a6bde4f5fc1a74da6b2c7adb4586c8383c
Will Campbell <sufuninja@gmail.com>
2016-05-27 11:03:38 -0500

    changed liks in navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -4,14 +4,14 @@
     <!--logo picture-->
         <a class="brand-logo"><img src="/img/adlister-logo-small.png"></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="homepage.html">Home</a></li>
-            <li><a href="index.php">Items</a></li>
-            <li><a href="account.php">Account</a></li>
-            <li><a href="login.php">Login</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/ads">Items</a></li>
+            <li><a href="/user/account">Account</a></li>
+            <li><a href="/user/login">Login</a></li>
     <!--unsure where the logout button takes the user  -->
-            <li><a href="logout.html">Log Out</a></li>
-            <li><a href="signup.php">Sign Up</a></li>
-            <li><a href="create.php">Post New Add</a></li>
+            <li><a href="#">Log Out</a></li>
+            <li><a href="/user/signup">Sign Up</a></li>
+            <li><a href="/ads/create">Post New Add</a></li>
         </ul>
     </div>
 </nav>
