f6054dd830764d1d4b8a5c67b2ed9bdfa9c961e9
kcates81 <kriscates81@gmail.com>
2016-05-27 10:49:22 -0500

    removed php and html body and head tags. the array that was in the page controller at the top of the page is now in /utils/helper_functions.php

--- views/ads/show.php
+++ views/ads/show.php
@@ -1,23 +1,4 @@
-<!--Page for single advertisement -->
-<?php
-function pageController()
-{
-    $item = ['title' => 'Jet', 'owner' => 'Sally', 'description' => 'lorem ipsum delor and other really neat things.', 'email' => 'sally89@gmail.com', 'address' => '1324 S. Lorem, San Antonio, Texas, 77777'];
-    return $item;
-}
-extract(pageController());
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <!-- Compiled and minified Materialize CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-    <title>Adlister | Item</title>
-</head>
-<body>
-  <?php //require "temp-navbar.php"; ?>
+<div class="container">
     <div>
         <h1 class="center-align"><?= $item['title'];?></h1>
     </div>
@@ -47,13 +28,8 @@ extract(pageController());
                 <button class="btn"><a href="">Delete ad</a></button>
             </div>
         </div>
-    </div>
+    </div>   
+</div>
+    
 
-    <?php //require 'temp-foot.php'; ?>
-
-    <!-- jQuery CDN -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-</body>
-</html>
+    
