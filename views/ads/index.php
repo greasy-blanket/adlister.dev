45923d95f40c9741147169992aaefdd8a7a1bcda
Will Campbell <sufuninja@gmail.com>
2016-05-31 12:29:33 -0500

    changed btn css

--- views/ads/index.php
+++ views/ads/index.php
@@ -12,20 +12,20 @@
             <?php if ($index % 4 == 0): ?>
                 <div class="row">
             <?php endif ?>
-                    <div class="col s12 m3">
-                        <div class="card medium">
-                            <div class="card-image">
-                                <img src="<?= $ad['img_url']; ?>" >
-                                <span class="card-title">Item <?= $ad['name'] ?></span>
-                            </div>
-                            <div class="card-content">
-                                <p><?= $ad['description']; ?></p>
-                            </div>
-                            <div class="card-action">
-                                <a href="/ads/show?user_id=<?= $ad['user_id']?>&id=<?= $ad['id']?>"><button class="btn">Details</button></a>
-                            </div>
+                <div class="col s12 m3">
+                    <div class="card medium">
+                        <div class="card-image">
+                            <img src="<?= $ad['img_url']; ?>" >
+                            <span class="card-title">Item <?= $ad['name'] ?></span>
+                        </div>
+                        <div class="card-content">
+                            <p><?= $ad['description']; ?></p>
+                        </div>
+                        <div class="card-action">
+                            <a href="/ads/show?user_id=<?= $ad['user_id']?>&id=<?= $ad['id']?>"><button class="btn">Details</button></a>
                         </div>
                     </div>
+                </div>
             <?php if ($index % 4 == 3): ?>
                 </div>
             <?php endif ?>
